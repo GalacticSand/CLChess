07bcfb62283e85836ed50ff3dfe36e65dd353be1
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-08-29 19:47:49 -0400

    Update clchess.cpp

--- clchess.cpp
+++ clchess.cpp
@@ -4,7 +4,16 @@
 #include <string>
 using namespace std;
 
+// DATA DIVISION
+
 bool running;
+int opt;
+string b_opt;
+
+string white_n;
+string black_n;
+vector<string> pcap_white;
+vector<string> pcap_black;
 
 vector<vector<string>> board;
 vector<vector<string>> def_board = {{"   ", " A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", "   "}, 
@@ -33,8 +42,59 @@ map<string, int> piece_map = {{"P", 1}, {"p", 1},
                               {"Q", 5}, {"q", 5}, 
                               {"K", 6}, {"k", 6}};
 
-void clearscr() { cout << "" << endl; }
+// MACRO DIVISON
+
+void clearscr() { cout << "\033[2J\033[1;1H"; }
 void pausescr() { cin.get(); }
+
+void renderscr(int scr)
+{
+    switch (scr)
+    {
+      case 1:
+        cout << "::::" << endl;
+        cout << "::::    Command Line Chess" << endl;
+        cout << "::::" << endl;
+        cout << "::::" << endl;
+        cout << "::::    Version 1.0.0" << endl;
+        cout << "::::    Copyright (C) 2020 - Galaxius Computer Software" << endl;
+        cout << "::::" << endl;
+        cout << endl;
+        cout << endl;
+        cout << "== Press Enter to Begin ==" << endl;
+        break;
+      case 2:
+        cout << endl;
+        cout << "Main Menu" << endl;
+        cout << endl;
+        cout << "1. Start New Game" << endl;
+        cout << "2. Options" << endl;
+        cout << "3. Credits" << endl;
+        cout << "4. Quit" << endl;
+        cout << endl;
+        cout << endl;
+        cout << "Select Option: ";
+        break;
+      case 3:
+        cout << endl;
+        cout << "" << endl;
+        break;
+      case 4:
+        cout << endl;
+        cout << "Game Credits" << endl;
+        cout << endl;
+        cout << "Command Line Chess" << endl;
+        cout << "Copyright (C) 2020 - Galaxius Computer Software" << endl;
+        cout << endl;
+        cout << endl;
+        cout << "== Press Enter to Continue ==" << endl;
+        break;
+      default:
+        break;
+    }
+}
+
+// PROCEDURAL DIVISION
 
 int main()
 {
